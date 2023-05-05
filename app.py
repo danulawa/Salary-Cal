ec753a528b83f2616e19c87437e41e4e4ae6f18c
danulawa <danula.wa@gmail.com>
2023-05-05 17:23:59 +0530

    add deductions and UI changes

--- app.py
+++ app.py
@@ -5,8 +5,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        basic_salary = int(request.form['basic_salary'])
-        allowances = int(request.form['total_allowances'])
+        basic_salary = request.form['basic_salary']
+        allowances = request.form['total_allowances']
+        deductions = request.form['total_deductions']
+
+        if not basic_salary:
+            basic_salary = 0
+
+        if not allowances:
+            allowances = 0
+
+        if not deductions:
+            deductions = 0
+
+        basic_salary = int(basic_salary)
+        allowances = int(allowances)
+        deductions = int(deductions)
+        
         annual_salary = (basic_salary + allowances) * 12
 
         if annual_salary in range(0, 1200001):
@@ -27,8 +42,12 @@ def home():
             income_tax = (500000 / 12) * 0.06 + (500000 / 12) * 0.12 + (500000 / 12) * 0.18 + (500000 / 12) * 0.24 + (
                         500000 / 12) * 0.30 + ((annual_salary - 3700000) / 12) * 0.36
 
-        net_salary = basic_salary * 92 / 100 + allowances - income_tax
-        return render_template('index.html', net_salary=net_salary)
+        net_salary = (basic_salary * 92 / 100 + allowances - income_tax) - deductions
+
+        net_salary = round(net_salary,2)
+        income_tax= round(income_tax,2)
+
+        return render_template('index.html', net_salary=net_salary, income_tax=income_tax)
 
     return render_template('index.html')
 

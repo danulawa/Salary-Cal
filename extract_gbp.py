31336b67fd1c41812fb68ac9aef9788a46ea52ed
danulawa <danula.wa@gmail.com>
2023-06-15 19:57:51 +0530

    file writing issue is fixed

--- extract_gbp.py
+++ extract_gbp.py
@@ -1,34 +1,35 @@
 import requests
 from PyPDF2 import PdfReader
+from io import BytesIO
 import re
 
 def extract_gbp():
-
     # Define the URL of the PDF
     pdf_url = "https://www.hsbc.lk/content/dam/hsbc/lk/documents/tariffs/foreign-exchange-rates.pdf"
 
     # Send a GET request to fetch the PDF content
     response = requests.get(pdf_url)
 
-    # Save the PDF locally
-    with open("exchange_rates.pdf", "wb") as file:
-        file.write(response.content)
+    # Open the PDF content in memory
+    pdf_file = BytesIO(response.content)
 
-    # Open the PDF file
-    with open("exchange_rates.pdf", "rb") as file:
-        pdf_reader = PdfReader(file)
+    # Open the PDF file from memory
+    pdf_reader = PdfReader(pdf_file)
 
-        # Extract text from each page
-        num_pages = len(pdf_reader.pages)
-        for page_num in range(num_pages):
-            page = pdf_reader.pages[page_num]
-            page_text = page.extract_text()
+    # Extract text from each page
+    num_pages = len(pdf_reader.pages)
+    for page_num in range(num_pages):
+        page = pdf_reader.pages[page_num]
+        page_text = page.extract_text()
 
-            # Search for the USD selling rate
-            if "Pound Sterling" in page_text:
-                selling_rate_index = page_text.index("Pound Sterling")
-                selling_rate_text = page_text[selling_rate_index:]
+        # Search for the USD selling rate
+        if "Pound Sterling" in page_text:
+            selling_rate_index = page_text.index("Pound Sterling")
+            selling_rate_text = page_text[selling_rate_index:]
 
-                matches = re.findall(r"\d+\.\d+", selling_rate_text)
+            matches = re.findall(r"\d+\.\d+", selling_rate_text)
 
-    return matches[1]
+            return matches[1]
+
+    # Return None if the USD selling rate is not found
+    return None
